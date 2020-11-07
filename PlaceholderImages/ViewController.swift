99b9d3325b8fc9e08dab3bdedfba1740ad4f02fd
Mohamed osama <mohamed.osama9812@gmail.com>
2020-11-06 21:53:37 -0800

    Initial Commit

---
+++ PlaceholderImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaceholderImages
+//
+//  Created by Mohamed on 11/6/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
